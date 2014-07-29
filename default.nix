9b9b5964c3241050aa76a23ae080dc8e07f81872
John Wiegley <johnw@newartisans.com>
2014-07-29 13:11:37 -0500

    Allow Haddocks for the Internal module to appear

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 
 cabal.mkDerivation (self: {
   pname = "taskpool";
-  version = "0.0.1";
+  version = "0.0.2";
   src = builtins.filterSource (path: type: type != "unknown") ./.;
   buildDepends = [ async fgl stm ];
   testDepends = [ hspec ];
