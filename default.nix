4d22835c2beca1fe7cf739ca5ff00f0d81a4f560
John Wiegley <johnw@newartisans.com>
2017-11-08 14:49:07 -0800

    Bump to 0.9.0.2

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "async-pool";
-  version = "0.9.0";
+  version = "0.9.0.2";
   src = ./.;
   buildDepends = [
     async base containers fgl monad-control stm transformers
